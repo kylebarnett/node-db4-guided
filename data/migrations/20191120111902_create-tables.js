b70801e2a171907be639002c43d9f142d392cb83
Kyle Barnett <kbarnett23@gmail.com>
2019-11-20 12:01:56 -0700

    had to do a rollback then migrate to latest, everything is up to par

--- data/migrations/20191120111902_create-tables.js
+++ data/migrations/20191120111902_create-tables.js
@@ -1,6 +1,6 @@
 
 exports.up = function (knex) {
-  return knex.schema.createTable('zooz', tbl => {
+  return knex.schema.createTable('zoos', tbl => {
     tbl.increments();
     tbl.string('zoo_name', 128).notNullable();
     tbl.string('address', 128).notNullable().unique();
